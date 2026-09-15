169f991835d2b9ffc47f2ddc8c55eb2719aa41fd
DouglasMotta95 <martatavares2023@gmail.com>
2026-09-15 09:26:25 -0300

    Wire asset radar into service worker

--- src/background-entry.js
+++ src/background-entry.js
@@ -4,4 +4,5 @@ import './background-device-anchor.js';
 import './background-market-session.js';
 import './background-modern-injector.js';
 import './background-shadow-calibration.js';
+import './background-asset-radar.js';
 import './background-control.js';
