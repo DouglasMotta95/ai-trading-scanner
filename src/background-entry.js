7fea668fac81ab2a90761e4be64362b9b3610e1b
DouglasMotta95 <martatavares2023@gmail.com>
2026-09-15 07:37:06 -0300

    refactor: use single market session background

--- src/background-entry.js
+++ src/background-entry.js
@@ -1,6 +1,5 @@
 import './services/scanner-state-atomic.js';
 import './background-build-guard.js';
-import './background-augment.js';
-import './background-integrity.js';
-import './background-chart-market.js';
+import './background-device-anchor.js';
+import './background-market-session.js';
 import './background.js';
