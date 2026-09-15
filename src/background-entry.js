c47688dba4bc5e3efcf32d9fa6594a2f30bb94be
DouglasMotta95 <martatavares2023@gmail.com>
2026-09-15 07:47:08 -0300

    feat: enable shadow calibration tracking

--- src/background-entry.js
+++ src/background-entry.js
@@ -3,4 +3,5 @@ import './background-build-guard.js';
 import './background-device-anchor.js';
 import './background-market-session.js';
 import './background-modern-injector.js';
+import './background-shadow-calibration.js';
 import './background.js';
