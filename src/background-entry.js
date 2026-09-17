bc9bf90180617bf3fa479d16a3d6aa1f6626aedb
DouglasMotta95 <martatavares2023@gmail.com>
2026-09-17 18:22:20 -0300

    fix: load acquisition and license recovery modules

--- src/background-entry.js
+++ src/background-entry.js
@@ -1,3 +1,5 @@
 import './services/scanner-state-atomic.js';
+import './background-license-guard.js';
+import './background-frame-relay-bootstrap.js';
 import './background-augment.js';
 import './background.js';
