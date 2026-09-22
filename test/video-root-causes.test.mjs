1a89b093e8ab8772cd0bf03b7a05ffc1cbed964e
DouglasMotta95 <martatavares2023@gmail.com>
2026-09-21 21:16:55 -0300

    v0.11.55 register video 15290 regression coverage

--- test/video-root-causes.test.mjs
+++ test/video-root-causes.test.mjs
@@ -1,5 +1,6 @@
-import { registerMarketContracts, registerTimeContracts, registerExpirationContracts } from './current-contracts.mjs';
+import { registerMarketContracts, registerTimeContracts, registerExpirationContracts, registerVideo15290Contracts } from './current-contracts.mjs';
 
 registerMarketContracts('video-root-causes');
 registerTimeContracts('video-root-causes');
 registerExpirationContracts('video-root-causes');
+registerVideo15290Contracts('video-15290');
